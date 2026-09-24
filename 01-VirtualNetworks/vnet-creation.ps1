272aaf850dda4d6625ceb7aec10e519cdd2174cb
eurico filho <euricoaafilho@gmail.com>
2026-09-24 11:23:08 -0300

    Ajusta script vnet-creation.ps1 para criar multiplas subnets

--- 01-VirtualNetworks/vnet-creation.ps1
+++ 01-VirtualNetworks/vnet-creation.ps1
@@ -14,7 +14,7 @@ $resourceGroupName = "rg-app-services"
 $vnetName = "KNET1"
 $location = "eastus"
 $addressPrefix = "10.0.0.0/16"
-$subnetName = "VM-Subnet"
+$subnetName = "VM-Subnet" # Esta é a primeira subnet que você cria
 $subnetPrefix = "10.0.1.0/24"
 
 # Criar Virtual Network
@@ -28,17 +28,48 @@ $vnet = New-AzVirtualNetwork `
 
 Write-Host "VNet criada!" -ForegroundColor Green
 
-# Criar Subnet
-Write-Host "Criando Subnet..." -ForegroundColor Yellow
+# Criar a primeira Subnet (VM-Subnet)
+Write-Host "Criando Subnet '$subnetName'..." -ForegroundColor Yellow
 
-$subnet = Add-AzVirtualNetworkSubnetConfig `
+$vnet = Add-AzVirtualNetworkSubnetConfig `
   -Name $subnetName `
   -VirtualNetwork $vnet `
-  -AddressPrefix $subnetPrefix
+  -AddressPrefix $subnetPrefix | Set-AzVirtualNetwork # Aplica a configuração da primeira subnet imediatamente
 
-$vnet | Set-AzVirtualNetwork
+Write-Host "Subnet '$subnetName' criada!" -ForegroundColor Green
 
-Write-Host "Subnet criada!" -ForegroundColor Green
+# Criar Multiplas SubNets na VNET
+Write-Host "Criando Subnets adicionais..." -ForegroundColor Yellow
+
+# Adicionando VM-Subnet1
+$vnet = Add-AzVirtualNetworkSubnetConfig `
+  -Name "VM-Subnet1" `
+  -VirtualNetwork $vnet `
+  -AddressPrefix "10.0.2.0/24" | Set-AzVirtualNetwork 
+
+Write-Host "Subnet 'VM-Subnet1' criada!" -ForegroundColor Green
+
+# Adicionando VM-Subnet2
+$vnet = Add-AzVirtualNetworkSubnetConfig `
+  -Name "VM-Subnet2" `
+  -VirtualNetwork $vnet `
+  -AddressPrefix "10.0.3.0/24" | Set-AzVirtualNetwork 
+
+Write-Host "Subnet 'VM-Subnet2' criada!" -ForegroundColor Green
+
+# Adicionando VM-Subnet3
+$vnet = Add-AzVirtualNetworkSubnetConfig `
+  -Name "VM-Subnet3" `
+  -VirtualNetwork $vnet `
+  -AddressPrefix "10.0.4.0/24" | Set-AzVirtualNetwork
+
+Write-Host "Subnet 'VM-Subnet3' criada!" -ForegroundColor Green
+Write-Host "Todas as subnets criadas!" -ForegroundColor Green
+
+# Listar VNETs (para verificar as subnets criadas)
+Write-Host "Listando informacoes da VNet '$vnetName'..." -ForegroundColor Cyan
+Get-AzVirtualNetwork -Name $vnetName -ResourceGroupName $resourceGroupName | Format-List Name, Location, AddressSpace, Subnets
+
 
 # Exibir informacoes
 Write-Host "Informacoes da VNet:" -ForegroundColor Cyan
